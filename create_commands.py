00546de1d9251849358a5f48825fec5accd2d3c3
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-06-18 01:47:19 +1000

    handling unknown commands

--- create_commands.py
+++ create_commands.py
@@ -1,11 +1,12 @@
 import logging
-from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler, filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
+# handle start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
     Sends a welcome message to the user.
@@ -21,6 +22,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     await context.bot.send_message(chat_id=chat_id, text=message)
 
+# repeat user message handler
 async def echo(update:Update, context: ContextTypes.DEFAULT_TYPE):
     """
     Asynchronous function that sends a message to a chat. 
@@ -35,9 +37,28 @@ async def echo(update:Update, context: ContextTypes.DEFAULT_TYPE):
     """
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+# turn inputs to caps command
 async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text_caps = ' '.join(context.args).upper()
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+# inline mode 
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper()
+            )))
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+# function to handle unknown commands
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token("6086120952:AAGoSxTW6iFidtqa_-SaP6I8TTZ-aFXN-6A").build()
@@ -45,9 +66,13 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     caps_handler = CommandHandler('caps', caps)
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
     application.add_handler(echo_handler)
     application.add_handler(start_handler)
     application.add_handler(caps_handler)
+    application.add_handler(inline_caps_handler)
+    application.add_handler(unknown_handler)
 
     application.run_polling()
