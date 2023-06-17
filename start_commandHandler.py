371bfcf355faa289412433990be0684badcad421
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-06-18 01:12:55 +1000

    create caps function that accepts words as list and converts them to capital letters

--- start_commandHandler.py
+++ start_commandHandler.py
@@ -35,12 +35,19 @@ async def echo(update:Update, context: ContextTypes.DEFAULT_TYPE):
     """
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token("6086120952:AAGoSxTW6iFidtqa_-SaP6I8TTZ-aFXN-6A").build()
 
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
 
     application.add_handler(echo_handler)
     application.add_handler(start_handler)
+    application.add_handler(caps_handler)
+
     application.run_polling()
